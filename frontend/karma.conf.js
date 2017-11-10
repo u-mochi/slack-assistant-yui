5bcb69cd12d2fcd231c37f47721d409144c91cf2
u-mochi <yuura.yoshizuki@gmail.com>
2017-11-10 17:04:16 +0900

    Support CircleCI 2.0

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -3,53 +3,12 @@ args.splice(0, 4);
 
 const polyfills = [];
 
-const files = polyfills.concat(args);
+var baseConfig = require('./karma.base.conf');
 
 module.exports = (config) => {
+  baseConfig(config);
   config.set({
-    basePath: '',
-    frameworks: ['jasmine'],
-    files: files,
-    preprocessors: {
-      '**/*.spec.ts': ['webpack'],
-      '**/*.spec.tsx': ['webpack']
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
-    },
-    webpack: {
-      resolve: {
-        extensions: ['.ts', '.js', ".tsx"]
-      },
-      module: {
-        rules: [
-          {
-            test: /\.tsx?$/,
-            use: [
-              {loader: "ts-loader"}
-            ]
-          }
-        ]
-      },
-      externals: {
-        'react/lib/ExecutionEnvironment': true,
-        'react/addons': true,
-        'react-addons-test-utils': true,
-        'react/lib/ReactContext': 'window',
-        'fs': '{}'
-      }
-    },
-    webpackMiddleware: {
-      stats: 'errors-only',
-      noInfo: true
-    },
-    reporters: ['mocha'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['Chrome'],
-    singleRun: true,
-    concurrency: Infinity
+    files: config.files.concat(args),
+    browsers: ['ChromeHeadless']
   })
 };
